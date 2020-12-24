438f7262ffe5c83ccaa9e1c75c8be970224d257c
Takashi Kanemoto <ttskch@gmail.com>
2020-12-24 16:32:36 +0900

    feat: 🎸 delete unnecessary files and tweak top page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -21,24 +18,5 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig');
-    }
-
-    /**
-     * @Route("/form", name="form")
-     */
-    public function form(Request $request)
-    {
-        $form = $this->createForm(ContactType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('success', 'Form is submitted.');
-
-            return $this->redirectToRoute('home_form');
-        }
-
-        return $this->render('home/form.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 }
